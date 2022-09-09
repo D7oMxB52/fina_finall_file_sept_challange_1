5c5482665dcaa528f3f0642b307bfc462c8e0695
D7oMxB52 <s3813024@student.rmit.edu.au>
2022-09-09 11:17:56 +1000

    Initial commit

---
+++ src/main/java/com/sept/abdulrahmanalanazi/rest/Septchallage1Application.java
@@ -0,0 +1,13 @@
+package com.sept.abdulrahmanalanazi.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Septchallage1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Septchallage1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sept/abdulrahmanalanazi/rest/Septchallage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sept.abdulrahmanalanazi.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Septchallage1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
